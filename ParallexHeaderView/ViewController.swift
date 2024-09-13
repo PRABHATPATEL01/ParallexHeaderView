574570dc862166801ff1dcd575009912f6743e4c
PRABHATPATEL01 <prabhatpatel301@gmail.com>
2024-09-13 13:47:18 +0530

    Initial Commit

---
+++ ParallexHeaderView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParallexHeaderView
+//
+//  Created by Naveen Singh on 13/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
